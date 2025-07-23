56264d9efed3be6946448f5aabb6bf977ea6475d
Sebastian Rojas <62036375+sebasrt8@users.noreply.github.com>
2025-07-23 19:05:25 -0400

    Fix: sync exact react and react-dom versions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
-import { redirect } from 'next/navigation';
+import { Metadata } from "next";
+import Hero from "@/components/Hero";
+import Brands from "@/components/Brands";
+import Feature from "@/components/Features";
+import About from "@/components/About";
+import FeaturesTab from "@/components/FeaturesTab";
+// import FunFact from "@/components/FunFact";
+// import Integration from "@/components/Integration";
+// import CTA from "@/components/CTA";
+import FAQ from "@/components/FAQ";
+import Pricing from "@/components/Pricing";
+import Contact from "@/components/Contact";
+// import Blog from "@/components/Blog";
+// import Testimonial from "@/components/Testimonial";
+import { integrations } from "@/integration.config";
 
-export const dynamic = 'force-dynamic';
+export const metadata: Metadata = {
+  title: "Visame",
+  description: "Visame República Dominicana",
+  // other metadata
+};
 
 export default function Home() {
-  redirect('/site');
+  return (
+    <main>
+      <Hero />
+      <Brands />
+      <Feature />
+      <About />
+      <FeaturesTab />
+      {/* <FunFact /> */}
+      {/* <Integration /> */}
+      {/* <CTA /> */}
+      <FAQ />
+      {/* <Testimonial /> */}
+      <Pricing />
+      <Contact />
+      {/* {integrations?.isSanityEnabled && <Blog />} */}
+    </main>
+  );
 }
