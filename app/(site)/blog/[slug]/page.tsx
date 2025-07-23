3b7abbef99aa3b4579e6b8f5517786e2881fc43d
Sebastian Rojas <62036375+sebasrt8@users.noreply.github.com>
2025-07-23 18:39:38 -0400

    Remove redirect and use site as root home

--- app/(site)/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BlogPostPage({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <h1>Blog post: {params.slug}</h1>
-    </div>
-  );
-}
